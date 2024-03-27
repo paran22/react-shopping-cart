f52f887a181553c7e7692b0b3649e057658638f5
gaeun <knospe1@naver.com>
2024-03-27 23:10:24 +0900

    feat: add queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,26 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
 import './styles/index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 async function enableMocking() {
 	if (process.env.NODE_ENV !== 'development') {
 		return;
 	}
-
-	const { worker } = await import('./mocks/browser');
-
-	// `worker.start()` returns a Promise that resolves
-	// once the Service Worker is up and ready to intercept requests.
+	const { worker } = await import('./api/mocks/browser.ts');
 	return worker.start();
 }
+
+const queryClient = new QueryClient();
 
 enableMocking().then(() => {
 	ReactDOM.createRoot(document.getElementById('root')!).render(
 		<React.StrictMode>
-			<RouterProvider router={router} />
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</React.StrictMode>,
 	);
 });
