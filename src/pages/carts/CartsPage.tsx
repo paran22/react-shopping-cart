c6284d15711e07bcfd5f432147b3690f4d45365c
gaeun <knospe1@naver.com>
2024-03-30 17:42:10 +0900

    feat: add PageTitle

--- src/pages/carts/CartsPage.tsx
+++ src/pages/carts/CartsPage.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import { useGetCarts } from '@api';
+import { PageTitle } from '@components';
 
 export default function CartsPage() {
-	return <div>CartPage</div>;
+	const { data: carts } = useGetCarts();
+	return (
+		<section className="cart-section">
+			<PageTitle title="장바구니" />
+		</section>
+	);
 }
