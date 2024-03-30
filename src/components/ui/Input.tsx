d59c9704eddd1153f6c90bf1ba153d741320df97
gaeun <knospe1@naver.com>
2024-03-30 23:54:55 +0900

    feat: add Input

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,7 @@
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	className?: string;
+}
+
+export default function Input({ className, ...props }: InputProps) {
+	return <input className={className} {...props} />;
+}
