27ae08c175f52cbcd2330a17b3e151a8e54d14db
gaeun <knospe1@naver.com>
2024-03-30 23:55:08 +0900

    fix: fix CheckBox

--- src/components/ui/CheckBox.tsx
+++ src/components/ui/CheckBox.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
-	label: string;
+	label?: string;
 }
 
 export default function Checkbox({ label, ...props }: CheckboxProps) {
 	return (
 		<div className="checkbox-container">
 			<input type="checkbox" className="checkbox" {...props} />
-			<label className="checkbox-label" htmlFor="checkbox">
-				{label}
-			</label>
+			{label && (
+				<label className="checkbox-label" htmlFor="checkbox">
+					{label}
+				</label>
+			)}
 		</div>
 	);
 }
