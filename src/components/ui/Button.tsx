55ded16c7b2d45cf897aa6209c5b6a813807ed60
gaeun <knospe1@naver.com>
2024-03-30 23:54:31 +0900

    fix: fix Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,17 +1,23 @@
+import clsx from 'clsx';
 import React from 'react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	children: React.ReactNode;
-	variant: 'primary' | 'border';
+	variant?: 'primary' | 'border';
+	className?: string;
 }
 
 export default function Button({
 	children,
-	variant = 'primary',
+	variant,
+	className,
 	...props
 }: ButtonProps) {
 	return (
-		<button className={`${variant}-button`} {...props}>
+		<button
+			className={clsx(variant && `${variant}-button`, className)}
+			{...props}
+		>
 			{children}
 		</button>
 	);
