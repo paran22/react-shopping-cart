21c671b6b0212fca188d84d3bca94bb971e86d56
gaeun <knospe1@naver.com>
2024-04-05 23:55:26 +0900

    feat: add ErrorView and LoadingView

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,3 +2,5 @@ export { default as Header } from './Header';
 export { default as PageTitle } from './PageTitle';
 export { default as NavItem } from './NavItem';
 export { default as Logo } from './Logo';
+export { default as ErrorView } from './ErrorView';
+export { default as LoadingView } from './LoadingView';
