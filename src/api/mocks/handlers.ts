38493cab371daf4f1bc36e39e449ada31fa558f9
gaeun <knospe1@naver.com>
2024-04-05 23:55:10 +0900

    fix: add delay in msw for test loading

--- src/api/mocks/handlers.ts
+++ src/api/mocks/handlers.ts
@@ -1,25 +1,29 @@
-import { HttpResponse, http } from 'msw';
+import { HttpResponse, delay, http } from 'msw';
 import mockDb from '../../../db.json';
 
 export const handlers = [
-  http.get('/products', () => {
+  http.get('/products', async () => {
+    await delay(1000);
     return HttpResponse.json({
       data: mockDb.products,
     });
   }),
-  http.get('/carts', () => {
+  http.get('/carts', async () => {
+    await delay(1000);
     return HttpResponse.json({
       data: mockDb.carts,
     });
   }),
-  http.delete('/carts/:cartId', (req) => {
+  http.delete('/carts/:cartId', async (req) => {
     const cartId = req.params.cartId;
+    await delay(1000);
     console.log('cartId', cartId);
     return HttpResponse.json({
       data: mockDb.carts.filter((cart) => cart.id !== Number(cartId)),
     });
   }),
-  http.get('/orders', () => {
+  http.get('/orders', async () => {
+    await delay(1000);
     return HttpResponse.json({
       data: mockDb.orders,
     });
