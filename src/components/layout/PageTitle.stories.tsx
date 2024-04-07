4901db494c430e40aed88142f15831f5889d2212
gaeun <knospe1@naver.com>
2024-04-07 22:35:30 +0900

    test: add PageTitle stories

---
+++ src/components/layout/PageTitle.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryFn } from '@storybook/react';
+
+import PageTitle from './PageTitle';
+
+export default {
+  title: 'Layout/PageTitle',
+  component: PageTitle,
+} as Meta<typeof PageTitle>;
+
+const Template: StoryFn<typeof PageTitle> = (args) => <PageTitle {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: '기본 제목',
+};
